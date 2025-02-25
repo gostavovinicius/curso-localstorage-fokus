727f073ee48db9610c68b73da6d0536365fdea4b
Gustavo Vinícius <88352305+gostavovinicius@users.noreply.github.com>
2025-02-25 15:37:56 -0300

    Editando Tarefas

--- script-crud.js
+++ script-crud.js
@@ -2,8 +2,13 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function atualizarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
@@ -23,8 +28,18 @@ function criarElementoTarefa(tarefa) {
 
     const botao = document.createElement('button')
     botao.classList.add('app_button-edit')
-    const imagemDoBotao = document.createElement('img')
 
+    botao.onclick = () => {
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?")
+        if(novaDescricao) {
+            paragrafo.textContent = novaDescricao
+            tarefa.descricao = novaDescricao
+            atualizarTarefas()
+        }
+ 
+    }
+
+    const imagemDoBotao = document.createElement('img')
     imagemDoBotao.setAttribute('src', '/imagens/edit.png')
 
     botao.append(imagemDoBotao)
@@ -40,6 +55,11 @@ btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
 })
 
+btnCancelar.addEventListener('click', () => {
+    textArea.value = ''
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
 formAdicionarTarefa.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const tarefa = {
@@ -48,7 +68,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    atualizarTarefas()
     textArea.value = ''
     formAdicionarTarefa.classList.toggle('hidden')
 })
